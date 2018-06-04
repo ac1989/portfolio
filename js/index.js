215edcbc470ec6f1f23e263993b371780d5b2e1d
ac1989 <welladjusted220@gmail.com>
2018-06-04 19:12:15 +0100

    rc1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import '../sass/index.scss';
+import './polyfills';
 
 window.onload = () => {
   console.log('hello world');
@@ -6,13 +7,23 @@ window.onload = () => {
   // Cache DOM
   const projects = document.getElementsByClassName('project-wrapper');
   const buttonWrappers = document.getElementsByClassName('project-buttons');
+  const techButtons = document.getElementsByClassName('tech-button');
 
+  // Variables
+  const colors = {
+    github: '#f2f2f2',
+    react: '#61dafb',
+    redux: '#744cbc',
+    'redux-saga': '#89d96d',
+    emotion: '#d26ac2',
+    mui: '#2196f3',
+    javascript: '#1a1a1a'
+  };
   let projectHeight = projects[0].getBoundingClientRect().height;
 
   [...projects].forEach(project => {
     const projectText = project.querySelector('.project-text');
     const techButtons = project.getElementsByClassName('tech-button');
-    console.log(techButtons);
 
     project.addEventListener('mouseenter', e => {
       e.stopPropagation();
@@ -30,6 +41,29 @@ window.onload = () => {
         techButton.style.transform = 'translateY(16px)';
         techButton.style.opacity = '0';
       });
+    });
+  });
+
+  [...techButtons].forEach(techButton => {
+    let tech = techButton.classList[1].substring(7);
+    let ripple = document.createElement('div');
+    ripple.style.opacity = 0;
+    ripple.style.background = colors[tech] || '#f2f2f2';
+    ripple.style.position = 'absolute';
+    ripple.style.left = 0;
+    ripple.style.width = '100%';
+    ripple.style.paddingTop = '100%';
+    ripple.style.top =
+      techButton.getBoundingClientRect().height -
+      ripple.getBoundingClientRect().height;
+    ripple.style.borderRadius = '50%';
+    ripple.style.animation = 'ripple 0.6s linear';
+
+    techButton.addEventListener('mouseenter', e => {
+      techButton.appendChild(ripple);
+    });
+    techButton.addEventListener('mouseleave', () => {
+      ripple.remove();
     });
   });
 
